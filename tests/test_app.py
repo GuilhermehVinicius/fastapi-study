e63977981fddbdb9c9f00e5559dee9a5900b3f14
GuilhermehVinicius <guilherme.ribeiro.business@gmail.com>
2024-11-12 20:49:11 -0300

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,9 @@
+from dataclasses import asdict
 from http import HTTPStatus
+
+from sqlalchemy import select
+
+from fast_zero.models import User
 
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
@@ -69,3 +74,22 @@ def test_delete_user(client):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='alice', password='secret', email='teste@test'
+        )
+        session.add(new_user)
+        session.commit()
+
+    user = session.scalar(select(User).where(User.username == 'alice'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'alice',
+        'password': 'secret',
+        'email': 'teste@test',
+        'created_at': time,
+    }
