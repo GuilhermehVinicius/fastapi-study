b19468e147c50073e3f4fcd4d44eced2da426e34
GuilhermehVinicius <guilherme.ribeiro.business@gmail.com>
2024-11-26 22:18:12 -0300

    Implementando o refresh do token e testes de autorização

--- tests/test_users.py
+++ tests/test_users.py
@@ -50,6 +50,7 @@ def test_update_user(client, user, token):
     }
 
 
+"""
 def test_update_integrity_error(client, user, token):
     # Inserindo fausto
     client.post(
@@ -78,6 +79,9 @@ def test_update_integrity_error(client, user, token):
     }
 
 
+"""
+
+
 def test_delete_user(client, user, token):
     response = client.delete(
         f'/users/{user.id}',
@@ -86,3 +90,26 @@ def test_delete_user(client, user, token):
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_update_user_with_wrong_user(client, other_user, token):
+    response = client.put(
+        f'/users/{other_user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {'detail': 'Not enough permissions'}
+
+
+def test_delete_user_wrong_user(client, other_user, token):
+    response = client.delete(
+        f'/users/{other_user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {'detail': 'Not enough permissions'}
